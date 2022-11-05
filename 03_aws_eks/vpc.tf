7769e48329440c9f92337b5d146406efc8d2d19d
yologger <yologger1013@gmail.com>
2022-11-06 02:07:26 +0900

    [ADD] Default Route Table, Default Security Group Naming

--- 03_aws_eks/vpc.tf
+++ 03_aws_eks/vpc.tf
@@ -1,6 +1,7 @@
 locals {
     vpc_name = "eks_vpc"
     default_route_table_name = "default_route_table"
+    default_security_group = "default_security_group"
 }
 
 # VPC 생성
@@ -11,10 +12,17 @@ resource "aws_vpc" "eks_vpc" {
   }
 }
 
-# VPC 생성 시 같이 생성되는 Default VPC에 이름 추가
+# VPC 생성 시 함께 생성되는 Route Table에 이름 추가
 resource "aws_default_route_table" "default_route_table" {
     default_route_table_id = aws_vpc.eks_vpc.default_route_table_id
     tags = { 
       Name = local.default_route_table_name 
     }
+}
+## VPC 생성 시 함께 생성되는 Securiy Group에 이름 추가
+resource "aws_default_security_group" "default_security_group" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags   = {
+    Name = local.default_security_group
+  }
 }
