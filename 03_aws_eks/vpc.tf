aa42d7ad9698f2373d99d13781a234eb88a51b5f
yologger <yologger1013@gmail.com>
2022-11-05 10:46:09 +0900

    [ADD] Default VPC 이름 변경

--- 03_aws_eks/vpc.tf
+++ 03_aws_eks/vpc.tf
@@ -1,6 +1,20 @@
+locals {
+    vpc_name = "eks_vpc"
+    default_route_table_name = "default_route_table"
+}
+
+# VPC 생성
 resource "aws_vpc" "eks_vpc" {
   cidr_block = var.cidr_block
   tags = {
-    Name = "eks_vpc"
+    Name = local.vpc_name
   }
+}
+
+# VPC 생성 시 같이 생성되는 Default VPC에 이름 추가
+resource "aws_default_route_table" "default_route_table" {
+    default_route_table_id = aws_vpc.eks_vpc.default_route_table_id
+    tags = { 
+      Name = local.default_route_table_name 
+    }
 }
