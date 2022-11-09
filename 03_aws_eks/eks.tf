9cf5c65f3575dabbabbee1e347f1c95c14c6a428
yologger <yologger1013@gmail.com>
2022-11-09 22:06:02 +0900

    [ADD] Terraform EKS 롤 추가

---
+++ 03_aws_eks/eks.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "eks_cluster" {
+  name = "${local.cluster_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
