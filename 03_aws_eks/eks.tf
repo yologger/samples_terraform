5356d5bd7b108fcdba003fc4f8eff4af2b9e864c
yologger <yologger1013@gmail.com>
2022-11-10 15:05:52 +0900

    [ADD] Terraform EKS 추가

--- 03_aws_eks/eks.tf
+++ 03_aws_eks/eks.tf
@@ -1,4 +1,5 @@
-resource "aws_iam_role" "eks_cluster" {
+## EKS Cluster가 사용할 Role 정의
+resource "aws_iam_role" "eks_cluster_role" {
   name = "${local.cluster_name}-role"
 
   assume_role_policy = jsonencode({
@@ -11,4 +12,28 @@ resource "aws_iam_role" "eks_cluster" {
       Action = "sts:AssumeRole"
     }]
   })
+}
+
+## Role에 Policy 추가
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+## EKS Cluster 생성
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = local.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  vpc_config {
+    subnet_ids = aws_subnet.private_subnets[*].id
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSVPCResourceController,
+  ]
 }
