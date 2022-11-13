f96de035ffe48bedcf4b74cc2522d4d06d7f19df
yologger <yologger1013@gmail.com>
2022-11-13 18:58:52 +0900

    [ADD] AWS ECR Repository 이름 변경

--- 03_aws_eks/ecr.tf
+++ 03_aws_eks/ecr.tf
@@ -2,7 +2,7 @@ locals {
   ecr_repositoy_name = "ecr_repository"
 }
 
-## simon-sample 앱을 위한 저장소를 만듭니다
-resource "aws_ecr_repository" "simon_sample" {
+// ECR Repository
+resource "aws_ecr_repository" "ecr_repository" {
   name = local.ecr_repositoy_name
 }
