0a6a4cee0e7e4e54c213ff92907d4d3efff6b2db
yologger <yologger1013@gmail.com>
2022-11-24 19:17:05 +0900

    [ADD] Helm 예제 추가

---
+++ 03_aws_eks/eks_with_terraform_module/vpc.tf
@@ -0,0 +1,27 @@
+locals {
+  cluster_name = "k8s-cluster"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "k8s-vpc"
+  cidr = "10.194.0.0/16"
+
+  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
+  public_subnets  = ["10.194.0.0/24", "10.194.1.0/24"]
+  private_subnets = ["10.194.100.0/24", "10.194.101.0/24"]
+
+  enable_nat_gateway     = true
+  one_nat_gateway_per_az = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
